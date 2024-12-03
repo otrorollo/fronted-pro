c65749e68c8cc30e3c21f7072ea6829478e68ee5
Nauel Gómez <ngomez@codiara.com>
2024-12-03 20:38:37 +0100

    Tipado de funciones

--- examples/typescript/1.variables.ts
+++ examples/typescript/1.variables.ts
@@ -1,6 +1,9 @@
+// Variables
 let outText = "Hola Mundo from typescript :)"
 const age: number = 23;
 const tel: number | string = '+34601001001';
+const response: unknown = {};
+const request: any = {};
 
 // Typescript es un lenguaje de tipo estricto.
 // Infiere los tipos iniciales y asume que siempre sera del mismo tipo.
