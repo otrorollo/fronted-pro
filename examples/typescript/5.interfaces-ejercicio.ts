96919b59eda4c0a5a8728cf87961fda59c39d5bc
Nauel Gómez <ngomez@codiara.com>
2024-12-03 21:58:42 +0100

    Ejercicio

---
+++ examples/typescript/5.interfaces-ejercicio.ts
@@ -0,0 +1,12 @@
+/**
+ * 
+ * Exercise 1
+ * Define una interfaz `Product` con las siguientes propiedades:
+ * - `name` de tipo `string`
+ * - `price` de tipo `number`
+ * - `description` de tipo `string`
+ * - `category` de tipo `string`
+ * 
+ * Define un array con 3 productos distintos.
+ * 
+ */
