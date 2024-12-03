60b699e2ff2821520c8e74912803ddfba3c0908c
Nauel Gómez <ngomez@codiara.com>
2024-12-03 20:18:56 +0100

    Tipado de variables

---
+++ examples/typescript/index.js
@@ -0,0 +1,7 @@
+var outText = "Hola Mundo from typescript :)";
+var age = 23;
+var tel = '+34601001001';
+// Typescript es un lenguaje de tipo estricto.
+// Infiere los tipos iniciales y asume que siempre sera del mismo tipo.
+// outText = 1; // -> error.
+console.log(outText);
