3c4caf3dc245e7d91f80999a1961c7d9b954b9a0
Nauel Gómez <ngomez@codiara.com>
2024-12-05 20:01:25 +0100

    extension de tipos

--- examples/typescript/5.interfaces-ejercicio.ts
+++ examples/typescript/5.interfaces-ejercicio.ts
@@ -45,10 +45,85 @@ console.table(products);
  * TODO:
  * Exercise 2
  * Define una interfaz addicional `Service` con las propiedades
- * customerName: string
- * price: number
- * periodicity: string
+ * customerName: string   OK
+ * price: number   OK
+ * periodicity: string   OK 
  * 
  * Define una función `findCheap` que reciba un array de tipo genérico y devuelva el elemento más barato.
  * Implementa esa función para buscar el elemento más barato de una lista de Products y el más barato de una lista de Services.
  */
+interface MinimalProduct {
+    price: number;
+}
+
+
+interface Service extends MinimalProduct {
+    customerName: string;
+    // price: number;
+    periodicity: string;
+}
+
+// { ...obj1, ...obj2 }
+
+function findCheap<T extends { price: number }>(elements: T[]): T {
+    return elements.reduce((prev, current) => prev.price < current.price ? prev : current);
+}
+
+
+
+// API
+const exampleResponseUsers = {
+    ts: 123456,
+    statusCode: 200,
+    data: [
+        // Array de usuarios
+    ]
+}
+
+const exampleResponseProducts = {
+    ts: 123456,
+    statusCode: 200,
+    data: [
+        // Array de productos
+    ]
+}
+
+// v1: genericos:
+interface ApiResponse<T> {
+    ts: number;
+    statusCode: number;
+    data: T
+}
+
+interface User {
+    id: number;
+    name: number;
+}
+
+const userResponse: ApiResponse<User[]> = {
+    ts: 123456,
+    statusCode: 200,
+    data: [
+        // Array de productos
+    ]
+}
+
+const statusResponse: ApiResponse<string> = {
+    ts: 123456,
+    statusCode: 200,
+    data: 'ok',
+}
+
+// v2: extension:
+interface ApiResponse2 {
+    ts: number;
+    statusCode: number;
+}
+
+interface UserResponse2 extends ApiResponse2 {
+    data: User[]
+}
+
+interface StatusResponse2 extends ApiResponse2 {
+    data: string;
+}
